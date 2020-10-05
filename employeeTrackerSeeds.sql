40994cad96fd4ded74ed3f2ccb8f8015d56235f6
Jada Arnett <jada.arnett@gmail.com>
2020-10-05 15:53:37 -0400

    added department row values

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -10,12 +10,19 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
+INSERT INTO department (name)
+VALUES ("Sales"),
+("Marketing"),
+("Research and Devlopment"),
+("Human Resources"),
+("Engineering");
+
 CREATE TABLE role (
      id INT NOT NULL AUTO_INCREMENT,
      title VARCHAR(30),
-     salary DECIMAL(10,2) NULL,
-    --  INT to hold reference to department role belongs to
+     salary DECIMAL(10,2),
      department_id INTEGER,
+     FOREIGN KEY (department_id) REFERENCES department(id)
      PRIMARY KEY (id)
 ); 
 
@@ -27,5 +34,8 @@ CREATE TABLE employee (
     role_id INT ,
     -- INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
     manager_id INT, 
+    is_manager BOOLEAN, 
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
     PRIMARY KEY (id)
 );
