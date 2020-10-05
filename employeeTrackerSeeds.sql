62d4a87e56e8654e21f314d1eddc50e461b92cd4
Jada Arnett <jada.arnett@gmail.com>
2020-10-05 16:25:40 -0400

    adding role salaries and names to role table

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -13,8 +13,10 @@ CREATE TABLE department (
 INSERT INTO department (name)
 VALUES ("Sales"),
 ("Marketing"),
+("Finance")
 ("Research and Devlopment"),
 ("Human Resources"),
+("Legal"),
 ("Engineering");
 
 CREATE TABLE role (
@@ -25,6 +27,20 @@ CREATE TABLE role (
      FOREIGN KEY (department_id) REFERENCES department(id)
      PRIMARY KEY (id)
 ); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), 
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 7),
+("Software Engineer", 120000, 7),
+("R&D Team Lead", 100000, 4),
+("HR Director", 90000, 5),
+("Account Manager", 150000, 3),
+("Lawyer", 190000, 6),
+("Accountant", 120000, 3),
+("Legal Team Lead", 250000, 6),
+("Marketing Consultant", 80000, 2),
+("Marketing Analyst", 125000, 2);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
