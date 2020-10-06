b754e765bf21f1da3f8a444a435bbbf719b96c67
Jada Arnett <jada.arnett@gmail.com>
2020-10-05 21:11:23 -0400

    added join to show all employees

--- employeeTrackerSeeds.sql
+++ employeeTrackerSeeds.sql
@@ -6,28 +6,28 @@ USE employeeDB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    department_name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES ("Sales"),
 ("Marketing"),
-("Finance")
+("Finance"),
 ("Research and Devlopment"),
 ("Human Resources"),
 ("Legal"),
 ("Engineering");
 
+
 CREATE TABLE role (
      id INT NOT NULL AUTO_INCREMENT,
      title VARCHAR(30),
-     salary DECIMAL(10,2),
+     salary DECIMAL(10,2) NULL,
      department_id INTEGER,
-     FOREIGN KEY (department_id) REFERENCES department(id)
+     FOREIGN KEY (department_id) REFERENCES department(id),
      PRIMARY KEY (id)
 ); 
-
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1), 
 ("Salesperson", 80000, 1),
@@ -46,12 +46,28 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    -- INT to hold reference to role employee has
     role_id INT ,
-    -- INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-    manager_id INT, 
-    is_manager BOOLEAN, 
+    manager VARCHAR (30),
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id),
     PRIMARY KEY (id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("Jim", "Halpert", 1, "John Doe"),
+("Dwight", "Schrute", 2, "Mike Chan"),
+("Creed", "Bratton", 5, null),
+("Stanley", "Hudson", 7, "Kevin Tupik"), 
+("Meredith", "Palmer", 2, "Malia Brown"),
+("Toby", "Flenderson", 4, "Ashley Rodriguez"),
+("Karen", "Filipelli", 8, "Sarah Lourd"),
+("Gabe", "Lewis", 12, "John Doe"),
+("Robert", "California", 11, null),
+("Oscar", "Nunez", 3, null);
+
+
+
+
+SELECT employee.first_name, employee.last_name, role.title, department.department_name, role.salary, employee.manager FROM role
+INNER JOIN employee ON employee.role_id = role.id 
+INNER JOIN department ON role.department_id = department.id;
